01022f708915595877ef487352f67b0287ec4671
jhmaestret <55798611+jhmaestret@users.noreply.github.com>
2021-10-06 21:55:13 -0500

    ajuste object

--- authApp/serializers/UserSerializer.py
+++ authApp/serializers/UserSerializer.py
@@ -3,8 +3,10 @@ from authApp.models.user import User
 from authApp.models.account import Account
 from authApp.serializers.AccountSerializer import AccountSerializer
 
+
 class UserSerializer(serializers.ModelSerializer):
     account = AccountSerializer()
+
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'name', 'email', 'account']
@@ -12,21 +14,21 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         accountData = validated_data.pop('account')
         userInstance = User.objects.create(**validated_data)
-        Account.objects.create(user=userInstance, **accountData)
+        Account.user.objects.create(user=userInstance, **accountData)
         return userInstance
 
     def to_representation(self, obj):
         user = User.objects.get(id=obj.id)
-        account = Account.objects.get(user=obj.id)
+        account = Account.user.objects.get(user=obj.id)
         return {
-                    'id': user.id,
-                    'username': user.username,
-                    'name': user.name,
-                    'email': user.email,
-                    'account': {
-                        'id': account.id,
-                        'balance': account.balance,
-                        'lastChangeDate': account.lastChangeDate,
-                        'isActive': account.isActive
-                    }
-                }
+            'id': user.id,
+            'username': user.username,
+            'name': user.name,
+            'email': user.email,
+            'account': {
+                'id': account.id,
+                'balance': account.balance,
+                'lastChangeDate': account.lastChangeDate,
+                'isActive': account.isActive
+            }
+        }
