cf1224b51a0d07f3f34c13dffd53669c29865ff9
jhmaestret <55798611+jhmaestret@users.noreply.github.com>
2021-10-06 22:56:42 -0500

    ajuste object

--- authApp/serializers/UserSerializer.py
+++ authApp/serializers/UserSerializer.py
@@ -14,12 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         accountData = validated_data.pop('account')
         userInstance = User.objects.create(**validated_data)
-        Account.user.objects.create(user=userInstance, **accountData)
+        Account.objects.create(user=userInstance, **accountData)
         return userInstance
 
     def to_representation(self, obj):
         user = User.objects.get(id=obj.id)
-        account = Account.user.objects.get(user=obj.id)
+        account = Account.objects.get(user=obj.id)
         return {
             'id': user.id,
             'username': user.username,
